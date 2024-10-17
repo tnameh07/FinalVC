a8df40581b48fde381110d0238633f4a40709229
tnameh07 <hdevde2611@gmail.com>
2024-10-17 11:03:15 +0530

    first commit

---
+++ backend/middilware/authentication.js
@@ -0,0 +1,40 @@
+
+// export default auth;
+import jwt from 'jsonwebtoken';
+import User from '../model/UserModel.js';
+
+const auth = async (req, res, next) => {
+    try {
+        console.log("Auth middleware activated");
+        const authHeader = req.headers['authorization'];
+        console.log("Authorization Header:", authHeader);
+
+        if (!authHeader) {
+            throw new Error('No token provided');
+        }
+
+        const token = authHeader.split(' ')[1]; // Extract token from "Bearer <token>"
+        console.log("Token:", token);
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("Decoded Token:", decoded);
+
+        const user = await User.findOne({ _id: decoded.userId });
+        console.log("User:", user);
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        req.token = token;
+        req.user = user;
+
+        console.log("Proceeding to next middleware");
+        next();
+    } catch (error) {
+        console.log("Authentication Error:", error.message);
+        res.status(401).json({ error: 'Please authenticate.' });
+    }
+};
+
+export default auth;
